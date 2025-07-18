33c15b7cba7db457278fc93b51fafb1bc02de8d2
Mickael <mickael.mesloub@gmail.com>
2025-07-18 19:47:27 +0200

    configuring beans using annotations

--- src/main/java/com/codewithmosh/store/OrderService.java
+++ src/main/java/com/codewithmosh/store/OrderService.java
@@ -1,12 +1,14 @@
 package com.codewithmosh.store;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
-
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void setPaymentService(PaymentService paymentService) {
         this.paymentService = paymentService;
