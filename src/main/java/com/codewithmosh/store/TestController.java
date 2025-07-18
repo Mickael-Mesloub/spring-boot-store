5ab13473bf1e03cbbfe1c82a70b22598205c0f75
Mickael <mickael.mesloub@gmail.com>
2025-07-18 18:58:15 +0200

    add test.html + Controller

--- src/main/java/com/codewithmosh/store/TestController.java
+++ src/main/java/com/codewithmosh/store/TestController.java
@@ -1,4 +1,13 @@
 package com.codewithmosh.store;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("Test page rendered");
+        return "test.html";
+    }
 }
